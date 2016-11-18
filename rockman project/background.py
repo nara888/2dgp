d5d817783bb5148be5b7193e209ed4d5436b4b3e
nara888 <nara-888@hanmail.net>
2016-11-18 16:16:13 +0900

    title, stage bgm

--- rockman project/background.py
+++ rockman project/background.py
@@ -5,6 +5,9 @@ from pico2d import *
 class Background:
     def __init__(self):
         self.image = load_image('resource/stage/stage.png')
+        self.bgm = load_wav('resource/bgm/JetManStage.wav')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
 
     def draw(self):
         self.image.draw(400, 350, 800, 700)
